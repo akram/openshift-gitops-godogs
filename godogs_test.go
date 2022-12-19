9553b0bfeca9fc50c33b88c86bffdcaefba6d6bc
Akram Ben Aissi <akram.benaissi@gmail.com>
2022-12-19 17:22:55 +0100

    first commit

---
+++ godogs_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+)
+
+var opts = godog.Options{Output: colors.Colored(os.Stdout)}
+
+func init() {
+	godog.BindCommandLineFlags("godog.", &opts)
+}
+
+func TestMain(m *testing.M) {
+	pflag.Parse()
+	opts.Paths = pflag.Args()
+
+	status := godog.TestSuite{
+		Name:                "godogs",
+		ScenarioInitializer: InitializeScenario,
+		Options:             &opts,
+	}.Run()
+
+	os.Exit(status)
+}
+
+func thereAreGodogs(available int) error {
+	Godogs = available
+	return nil
+}
+
+func iEat(num int) error {
+	err := assertExpectedAndActual(
+		assert.GreaterOrEqual, Godogs, num,
+		"You cannot eat %d godogs, there are %d available", num, Godogs,
+	)
+	if err != nil {
+		return err
+	}
+
+	Godogs -= num
+	return nil
+}
+
+func thereShouldBeRemaining(remaining int) error {
+	return assertExpectedAndActual(
+		assert.Equal, Godogs, remaining,
+		"Expected %d godogs to be remaining, but there is %d", remaining, Godogs,
+	)
+}
+
+func thereShouldBeNoneRemaining() error {
+	return assertActual(
+		assert.Empty, Godogs,
+		"Expected none godogs to be remaining, but there is %d", Godogs,
+	)
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+		Godogs = 0 // clean the state before every scenario
+		return ctx, nil
+	})
+
+	ctx.Step(`^there are (\d+) godogs$`, thereAreGodogs)
+	ctx.Step(`^I eat (\d+)$`, iEat)
+	ctx.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+	ctx.Step(`^there should be none remaining$`, thereShouldBeNoneRemaining)
+}
+
+// assertExpectedAndActual is a helper function to allow the step function to call
+// assertion functions where you want to compare an expected and an actual value.
+func assertExpectedAndActual(a expectedAndActualAssertion, expected, actual interface{}, msgAndArgs ...interface{}) error {
+	var t asserter
+	a(&t, expected, actual, msgAndArgs...)
+	return t.err
+}
+
+type expectedAndActualAssertion func(t assert.TestingT, expected, actual interface{}, msgAndArgs ...interface{}) bool
+
+// assertActual is a helper function to allow the step function to call
+// assertion functions where you want to compare an actual value to a
+// predined state like nil, empty or true/false.
+func assertActual(a actualAssertion, actual interface{}, msgAndArgs ...interface{}) error {
+	var t asserter
+	a(&t, actual, msgAndArgs...)
+	return t.err
+}
+
+type actualAssertion func(t assert.TestingT, actual interface{}, msgAndArgs ...interface{}) bool
+
+// asserter is used to be able to retrieve the error reported by the called assertion
+type asserter struct {
+	err error
+}
+
+// Errorf is used by the called assertion to report an error
+func (a *asserter) Errorf(format string, args ...interface{}) {
+	a.err = fmt.Errorf(format, args...)
+}
